2b66a68437f12e9759478b8c5ab600754f5585df
ramirezj <ramirezj@users.noreply.github.com>
2019-11-24 11:26:56 +0000

    fix(toolchain): Convert eslint configuration to JSON for Hound to pickup

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  globals: {
-    __PATH_PREFIX__: true,
-  },
-  "parserOptions": { "ecmaVersion": 6 },
-  plugins: ["sonarjs"],
-  extends: ["react-app","plugin:sonarjs/recommended"],
-}
